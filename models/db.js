8a28845c7e3eabc0c8178bdc02d07a2b6487be51
iamsurajshinde <surajshindecse@gmail.com>
2024-09-10 09:27:15 +0530

    change in query

--- models/db.js
+++ models/db.js
@@ -9,35 +9,35 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     // throw err;
   } else {
     console.log("Connected to the SQLite database.");
-    // db.exec(
-    //   `CREATE TABLE users (
-    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    //         name text,
-    //         username text UNIQUE, 
-    //         email text UNIQUE, 
-    //         password text,
-    //         CONSTRAINT email_unique UNIQUE (email),
-    //         CONSTRAINT username_unique UNIQUE (username)
-    //     );`,
-    //   (err) => {
-    //     if (err) {
-    //       console.error(err);
-    //     }
-    //   }
-    // );
+    db.exec(
+      `CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text,
+            username text UNIQUE, 
+            email text UNIQUE, 
+            password text,
+            CONSTRAINT email_unique UNIQUE (email),
+            CONSTRAINT username_unique UNIQUE (username)
+        );`,
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
 
-    // db.exec(
-    //   `CREATE TABLE admins (
-    //     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    //     adminId INTEGER,
-    //     FOREIGN KEY(adminId) REFERENCES users(id)
-    //     );`,
-    //   (err) => {
-    //     if (err) {
-    //         console.error(err);
-    //     }
-    //   }
-    // );
+    db.exec(
+      `CREATE TABLE IF NOT EXISTS admins (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        adminId INTEGER,
+        FOREIGN KEY(adminId) REFERENCES users(id)
+        );`,
+      (err) => {
+        if (err) {
+            console.error(err);
+        }
+      }
+    );
   }
 });
 
